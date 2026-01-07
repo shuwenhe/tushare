379f3ec1c610fde981b2a30888883ba8a178b0b5
shuwen <1201220707@pku.edu.cn>
2026-01-08 02:23:07 +0800

    feat:stock day data

---
+++ docs/stock_day_data.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE IF NOT EXISTS quant;
+
+CREATE TABLE quant.stock_day_data (
+    ts_code String,
+    trade_date String,
+    open Float64,
+    high Float64,
+    low Float64,
+    close Float64,
+    pre_close Float64,
+    change Float64,
+    pct_chg Float64,
+    vol Float64,
+    amount Float64,
+    date_stamp Float64
+) ENGINE = MergeTree()
+ORDER BY (ts_code, trade_date);
